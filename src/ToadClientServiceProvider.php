251a7de544416ca917d169bf6acecff9e44f5f9d
CLEMREP <clement@web-id.fr>
2022-11-04 16:39:05 +0100

    :bug: Remove comments

--- src/ToadClientServiceProvider.php
+++ src/ToadClientServiceProvider.php
@@ -10,11 +10,6 @@ class ToadClientServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('toad-client')
             ->hasConfigFile()
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,30 +2,15 @@
 
 namespace WebId\ToadClient\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
 use WebId\ToadClient\ToadClientServiceProvider;
 
 class TestCase extends Orchestra
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'WebId\\ToadClient\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
-    }
-
     protected function getPackageProviders($app)
     {
         return [
             ToadClientServiceProvider::class,
         ];
-    }
-
-    public function getEnvironmentSetUp($app)
-    {
-        config()->set('database.default', 'testing');
     }
 }
